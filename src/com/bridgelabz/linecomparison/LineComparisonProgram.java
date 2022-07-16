2d598d4d635a2416f74ba76b095e25c1bb7d5b23
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-16 14:49:28 +0530

    Modified: Equals Method

--- src/com/bridgelabz/linecomparison/LineComparisonProgram.java
+++ src/com/bridgelabz/linecomparison/LineComparisonProgram.java
@@ -13,8 +13,25 @@ public class LineComparisonProgram {
         int x2=scr.nextInt();
         int y2=scr.nextInt();
 
-        double length=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        System.out.println("Length of line is: "+length);
+        Double lengthOfLineOne=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        System.out.println("Length of line1 is: "+lengthOfLineOne);
+
+        System.out.println("Enter x3 and y3 co-ordinates of first point");
+        int x3=scr.nextInt();
+        int y3=scr.nextInt();
+        System.out.println("Enter x4 and y4 co-ordinates of second point");
+        int x4=scr.nextInt();
+        int y4=scr.nextInt();
+        Double lengthOfLineTwo=Math.sqrt(Math.pow(x3-x4,2)+Math.pow(y3-y4,2));
+        System.out.println("Length of line2 is: "+lengthOfLineTwo);
+
+        if(lengthOfLineOne.equals(lengthOfLineTwo)){
+            System.out.println("line1 and line2 are equal");
+        }
+        else{
+            System.out.println("line1 and line2 are not equal");
+        }
+
 
     }
 }
