9860d500ef682c6b5443d7b4262ccdba6af75c7c
yasinnadaf <106942238+yasinnadaf@users.noreply.github.com>
2022-08-12 20:27:32 +0530

    Update LineComparisonProgram.java

--- src/com/bridgelabz/linecomparison/LineComparisonProgram.java
+++ src/com/bridgelabz/linecomparison/LineComparisonProgram.java
@@ -3,17 +3,18 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparisonProgram {
+    
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
-        Scanner scr=new Scanner(System.in);
+        Scanner scr = new Scanner(System.in);
         System.out.println("Enter x1 and y1 Co-ordinates of first point");
-        int x1=scr.nextInt();
-        int y1=scr.nextInt();
+        int x1 = scr.nextInt();
+        int y1 = scr.nextInt();
         System.out.println("Enter x2 and y2 co-ordinates of second point");
-        int x2=scr.nextInt();
-        int y2=scr.nextInt();
+        int x2 = scr.nextInt();
+        int y2 = scr.nextInt();
 
-        double length=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
         System.out.println("Length of line is: "+length);
 
     }
